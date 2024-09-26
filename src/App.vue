acd2afe18be784b39c3c3e92c4bbbbe2bf7d8694
Marina Kholodova <marinaholodova@gmail.com>
2024-09-26 11:37:59 +0300

    feat: implement todo management with add, edit, delete, and toggle functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import {ITodoItem, useTodoStore} from "./store/todoStore.ts";
+import {computed, onMounted, ref} from "vue";
+
+const todoStore = useTodoStore();
+
+const todos = computed(() => todoStore.todos);
+const startEdit = ref<number | null>(null);
+const newTodoText = ref('');
+const editTodoText = ref('');
+
+const addNewTodo = (): void => {
+  if (newTodoText.value.trim()) {
+    todoStore.addTodo(newTodoText.value);
+    newTodoText.value = '';
+  }
+};
+
+const deleteNewTodo = (id: number): void => {
+  todoStore.deleteTodo(id);
+}
+
+const editTodo = (todo: ITodoItem): void => {
+  startEdit.value = todo.id;
+  editTodoText.value = todo.text;
+}
+const saveEdit = (todo: ITodoItem): void => {
+  todoStore.editTodo(todo.id, editTodoText.value);
+  cancelEdit();
+}
+const cancelEdit = (): void => {
+  startEdit.value = null;
+  editTodoText.value = ''
+}
+
+const toggleTodo = (id: number): void => {
+  todoStore.toggleTodo(id);
+}
+
+onMounted(() => {
+  todoStore.getTodos();
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="todo-list">
+    <h1>To-Do List</h1>
+
+    <input v-model="newTodoText"
+           placeholder="Add task.."
+           @keyup.enter="addNewTodo"/>
+    <button @click="addNewTodo">Add</button>
+
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <div v-if="startEdit===todo.id">
+          <input v-model="editTodoText"
+                 @keyup.enter="saveEdit(todo)"/>
+<!--          <button @click="saveEdit(todo)">Save</button>-->
+          <button @click="cancelEdit">Cancel</button>
+        </div>
+        <div v-else>
+          <input :checked="todo.completed" type="checkbox" @change="toggleTodo(todo.id)"/>
+          <span :class="{'completed':todo.completed} " @click="editTodo(todo)">{{ todo.text }}</span>
+          <button @click="editTodo(todo)">Edit</button>
+          <button @click="deleteNewTodo(todo.id)">&times;</button>
+        </div>
+
+      </li>
+
+    </ul>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.completed {
+  text-decoration: line-through;
+  color: grey;
 }
 </style>
